53d6c0813a7d0c5a7f317d4c651f89879153053f
Roberto Cervantes <robece@protonmail.com>
2020-08-07 14:09:15 -0500

    Updated Moodle's AKS configuration

--- source/aks/scripts.md
+++ source/aks/scripts.md
@@ -3,7 +3,8 @@ Moodle Helm Chart install with custom image:
 
 ```
 
-helm upgrade --install moodle bitnami/moodle --set image.repository=robecehub/moodle --set image.tag=1.0.36 --set replicaCount=1 --namespace=moodle --set externalDatabase.host=moodledb02.mysql.database.azure.com --set externalDatabase.port=3306 --set externalDatabase.database=moodle --set externalDatabase.user=moodleuser --set externalDatabase.password=Password.123 --set moodleSkipInstall=yes --set mariadb.enabled=false --set moodleUsername=admin --set moodlePassword=Password.123 --set moodleEmail=robece@protonmail.com --set nodeSelector\.kubernetes\.io/os=linux --set service.type=LoadBalancer --set extraEnvVars[0].name=MOODLE_DATABASE_TYPE --set extraEnvVars[0].value=mysqli --set livenessProbe.enabled=false --set readinessProbe.enabled=false --set persistence.enabled=true --set persistence.existingClaim=pvc-nfs
+
+helm upgrade --install moodle bitnami/moodle --set image.repository=robecehub/moodle --set image.tag=1.0.38 --set replicaCount=1 --namespace=moodle --set externalDatabase.host=moodledb02.mysql.database.azure.com --set externalDatabase.port=3306 --set externalDatabase.database=moodle --set externalDatabase.user=moodleuser --set externalDatabase.password=Password.123 --set moodleSkipInstall=yes --set mariadb.enabled=false --set moodleUsername=admin --set moodlePassword=Password.123 --set moodleEmail=robece@protonmail.com --set nodeSelector\.kubernetes\.io/os=linux --set service.type=LoadBalancer --set extraEnvVars[0].name=MOODLE_DATABASE_TYPE --set extraEnvVars[0].value=mysqli --set livenessProbe.enabled=false --set readinessProbe.enabled=false --set persistence.enabled=true --set persistence.existingClaim=pvc-nfs
 
 
 
---
+++ source/moodle/3.9.1-debian-10-r17/patch/moodle/config.php
@@ -0,0 +1,55 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'moodledb02.mysql.database.azure.com';
+$CFG->dbname    = 'moodle';
+$CFG->dbuser    = 'moodleuser';
+$CFG->dbpass    = 'Password.123';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => 3306,
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_unicode_ci',
+);
+
+if (empty($_SERVER['HTTP_HOST'])) {
+  $_SERVER['HTTP_HOST'] = '127.0.0.1:8080';
+}
+if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
+  $CFG->wwwroot   = 'https://' . $_SERVER['HTTP_HOST'];
+} else {
+  $CFG->wwwroot   = 'http://' . $_SERVER['HTTP_HOST'];
+}
+$CFG->dataroot  = '/bitnami/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 02775;
+
+$CFG->emailconnectionerrorsto = 'admin@moodle.com';
+$CFG->tempdir = '/bitnami/moodledata/temp'; // Directory MUST BE SHARED by all cluster nodes.
+$CFG->cachedir = '/bitnami/moodledata/cache'; // Directory MUST BE SHARED by all cluster nodes, locking required.
+$CFG->localcachedir = '/tmp'; // Intended for local node caching.
+$CFG->preventfilelocking = true;
+$CFG->lock_factory = "\core\lock\db_record_lock_factory";
+
+$CFG->session_handler_class = '\core\session\redis';
+$CFG->session_redis_host = '52.137.106.33';
+$CFG->session_redis_port = 6379;  // Optional.
+$CFG->session_redis_database = 0;  // Optional, default is db 0.
+$CFG->session_redis_prefix = 'moodle_session_'; // Optional, default is don't set one.
+$CFG->session_redis_acquire_lock_timeout = 120;
+$CFG->session_redis_lock_expire = 7200;
+$CFG->session_redis_serializer_use_igbinary = true; // Optional, default is PHP builtin serializer.
+
+define('CONTEXT_CACHE_MAX_SIZE', 7500);
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
